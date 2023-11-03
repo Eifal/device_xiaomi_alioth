b93999d09f5e856d93d2ef82043ebd5f9fc5e9bc
eifal <syahrifal7@gmail.com>
2023-11-03 15:45:22 +0700

    bringup evolutionx

--- evolution_alioth.mk
+++ evolution_alioth.mk
@@ -9,16 +9,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common DerpFest stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-#Derp Stuffs
-USE_LEGACY_BOOTANIMATION := true
-DERP_BUILDTYPE := Official
+#Evox stuff
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := derp_alioth
+PRODUCT_NAME := evolution_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
